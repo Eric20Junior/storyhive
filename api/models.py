7ad412e948f3ecd4cc9d5423f98af41ba895f0ec
Eric20Junior <eric20junior@gmail.com>
2023-03-20 23:25:49 +0100

    worked on the models,admin & serializer

---
+++ api/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+    
+class Post(models.Model):
+
+    class PostObjects(models.Manager):
+        def get_queryset(self):
+            return super().get_queryset().filter(status=1)
+
+    STATUS = (
+        (0, 'Draft'),
+        (1, 'Published'),
+    )
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, unique=True)
+    description = models.TextField(max_length=100)
+    body = models.TextField()
+    slug = models.SlugField(max_length=255, unique=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, default=1)
+    status = models.IntegerField(choices=STATUS, default=0)
+    created_on = models.DateTimeField(auto_now_add=True)
+    publish_date = models.DateTimeField(blank=True, null=True)
+    published = models.BooleanField(default=False)
+    objects = models.Manager() # default manager.
+    postobjects = PostObjects() # custom manager.
+
+    class Meta:
+        ordering = ['-publish_date']
+
+    def __str__(self):
+        return self.title

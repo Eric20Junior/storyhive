de25d1d2deefdaffa280f6a4d5ee4cb06d1a41dc
Eric20Junior <eric20junior@gmail.com>
2023-03-22 10:29:43 +0100

    created a url path for comments

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import BookListView, BookDetailView,BookCreateView, BookUpdateView, BookDeleteView
+from .views import BookListView, BookDetailView,BookCreateView, BookUpdateView, BookDeleteView, CommentList, CommentDetail
 from . import views
 
 urlpatterns = [
@@ -8,6 +8,9 @@ urlpatterns = [
    path('book-detail/<int:pk>/', BookDetailView.as_view(), name='book-detail'), 
    path('create-book/', BookCreateView.as_view(), name='create-book'),
    path('update-book/<int:pk>/', BookUpdateView.as_view(), name='update-book'), 
-   path('delete-book/<int:pk>/', BookDeleteView.as_view(), name='delete-book'), 
+   path('delete-book/<int:pk>/', BookDeleteView.as_view(), name='delete-book'),
 
+
+    path('comments/', CommentList.as_view(), name='comments'),
+    path('comments/<int:pk>/', CommentDetail.as_view(), name='comment-detail'),
 ]

c0c9492715065e4f840b1a0df3e2dd2174fe80d2
Eric20Junior <eric20junior@gmail.com>
2023-03-22 10:17:44 +0100

    created a comment serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
-from .models import Post
+from .models import Post, Comment
 
 class PostSerializer(serializers.ModelSerializer):
+    """Post Serializer"""
     class Meta:
         model = Post
         exclude = ['publish_date']
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    """Comment serializer"""
+    class Meta:
+        model = Comment
+        exclude = ['post']
